cff625f1b6e190190b3fd752de44ecf2badad84c
Angelos Papamichail <55174356+angelospk@users.noreply.github.com>
2022-11-01 14:20:53 +0000

    Initial commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,42 @@
-def arithmetic_arranger(problems):
-
-
-    return arranged_problems
+import re
+def arithmetic_arranger(problems, show=False):
+  if (len(problems)>5):
+    return "Error: Too many problems."
+  reg1="^\s*\d+\s*\D\s*\d+"
+  reg2="^\s*\d{5,}\s*\D\s*\d{5,}"
+  reg3="^\s*\d+\s*[*\/]\s*\d+"
+  b=[]
+  for problem in problems:
+    if not (bool(re.match(reg1,problem))): 
+      return "Error: Numbers must only contain digits."
+    if (bool(re.match(reg2,problem))):
+      return "Error: Numbers cannot be more than four digits."
+    if (bool(re.match(reg3,problem))):
+      return "Error: Operator must be '+' or '-'."
+    b.append(problem.split(" "))
+  first=""
+  second=""
+  third=""
+  last=""
+  results=[]
+  #print(b)
+  for pr in b:
+    l=max(len(pr[0]),len(pr[2]))+2
+    #print (l)
+    text="{:>"+str(l)+"}"
+    first+=text.format(pr[0])+"    "
+    text2="{:>"+str(l-1)+"}"
+    second+=pr[1]+text2.format(pr[2])+"    "
+    third+="-"*l+"    "
+    if (pr[1]=="+"):
+      results=int(pr[0])+int(pr[2])
+    else:
+      results=int(pr[0])-int(pr[2])
+    last+=text.format(str(results))+"    "
+    #if (ind==len(b)):
+      #print
+      
+  ret=first[:len(first)-4]+"\n"+second[:len(second)-4]+"\n"+third[:len(third)-4]
+  if (show):
+    ret+="\n"+last[:len(last)-4]
+  return ret
